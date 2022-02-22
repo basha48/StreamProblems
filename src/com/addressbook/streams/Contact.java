e4ab040681e6781a6dc160598eb62ba50dfc2f19
basha <69071214+basha48@users.noreply.github.com>
2022-02-22 19:00:11 +0530

    Update Contact.java

--- src/com/addressbook/streams/Contact.java
+++ src/com/addressbook/streams/Contact.java
@@ -2,13 +2,13 @@ package com.addressbook.streams;
 
 public class Contact {
 		
-		 public String firstName;
-	     public String lastName ;
-	     public String phoneNumber; 
-	     public String address ;
-	     public String city ;
-	     public String state ;
-	     public String zip ;
-	     public String email;
+	public String firstName;
+	public String lastName ;
+	public String phoneNumber; 
+	public String address ;
+	public String city ;
+	public String state ;
+	public String zip ;
+	public String email;
 	     
 }
